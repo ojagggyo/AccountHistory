467a47c493eb52feba499b6e138539c7ebe56cca
ojagggyo <ojagggyo@gmail.com>
2022-12-04 14:32:18 +0900

    m

--- src/app.js
+++ src/app.js
@@ -24,6 +24,11 @@ function vestToSteem(vest){//★
 	// 	vest, 
 	// 	globalProperties.total_vesting_shares, 
 	// 	globalProperties.total_vesting_fund_steem)
+
+console.log(total_vesting_shares);
+console.log(total_vesting_fund_steem);
+console.log(k);
+console.log(sp);
 	
 	let total_vesting_shares = parseFloat(globalProperties.total_vesting_shares.replace(" VESTS", ""));
 	let total_vesting_fund_steem = parseFloat(globalProperties.total_vesting_fund_steem.replace(" STEEM", ""));
